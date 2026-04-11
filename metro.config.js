7b08f1810a06ad7bf6441cd7ba0df689baa192db
Aqu1bp <aquibp4@gmail.com>
2026-04-11 09:44:57 +0530

    Fix Expo Go crash: add reanimated babel plugin, fix stockfish asset loading
    - Add babel.config.js with react-native-reanimated/plugin (fixes HostFunction crash)
    - Rename stockfish JS to .bundle extension (Metro can't load .js as asset)
    - Add .bundle to metro assetExts
    - Update stockfish-runner.html to reference renamed file

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('tflite', 'wasm', 'html');
+config.resolver.assetExts.push('tflite', 'wasm', 'html', 'bundle');
 
 module.exports = config;
